e105b189599346570caf2dcc84bc9d0abb104f2e
Mori <MoriMail@mail.mail>
2024-09-25 15:14:55 +0700

    up to new 0.0.2 version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='UnityProxy',
-      version='0.0.1',
+      version='0.0.2',
       description='Proxy parser and converter',
       author='Lisica',
       long_description=long_description,
